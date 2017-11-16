d0392b94de61a223a1b996b127aab3ccbd1dacbd
oavm <oavm02@gmail.com>
2017-11-16 19:01:56 +0100

    Finished prediction for one image

--- CNNCpp/HyQNN.h
+++ CNNCpp/HyQNN.h
@@ -5,17 +5,22 @@ class NeuralNetwork
 {
   Eigen::Tensor<double, 4> K0;
   Eigen::Tensor<double, 4> K1;
-  Eigen::MatrixXd image,b0,b1;
+  Eigen::MatrixXd image,b0,b1,b2,b3,W0,W1;
 
 
 public:
-  void set_neuralNetwork (Eigen::Tensor<double, 4> a,Eigen::Tensor<double, 4> b, Eigen::MatrixXd c, Eigen::MatrixXd d, Eigen::MatrixXd e)
+  void set_neuralNetwork (Eigen::Tensor<double, 4> a,Eigen::Tensor<double, 4> b, Eigen::MatrixXd c, Eigen::MatrixXd d, Eigen::MatrixXd e,
+                          Eigen::MatrixXd f, Eigen::MatrixXd g, Eigen::MatrixXd h, Eigen::MatrixXd m)
   {
     K0 = a;
     K1 = b;
     b0 = c;
     b1 = d;
-    image = e;
+    b2 = e;
+    b3 = f;
+    W0 = g;
+    W1 = h;
+    image = m;
   }
 
   Eigen::MatrixXd extract2DMatrix(int index_0, int index_1,Eigen::Tensor<double, 4> tensorMatrix)
@@ -154,22 +159,26 @@ public:
     return filteredImage;
   }
 
-  Eigen::Tensor<double, 4> reLu()
+  Eigen::Tensor<double, 4> reLu(Eigen::Tensor<double, 4> filteredIm)
   {
     int kernelHeight,kernelWidth,imageHeight,imageWidth;
-    imageHeight = image.rows();
-    imageWidth = image.cols();
+    Eigen::MatrixXd sizeImage;
 
-    Eigen::Tensor<double, 4> posLinTensor(imageHeight,imageWidth,K0.dimension(2),K0.dimension(3));
-    Eigen::Tensor<double, 4> filteredIm(imageHeight,imageWidth,K0.dimension(2),K0.dimension(3));
+    sizeImage = extract2DMatrix(0,0,filteredIm);
 
-    filteredIm = convolution(image,K0);
+    imageHeight = sizeImage.rows();
+    imageWidth = sizeImage.cols();
+
+    Eigen::Tensor<double, 4> posLinTensor(imageHeight,imageWidth,filteredIm.dimension(2),filteredIm.dimension(3));
+    // Eigen::Tensor<double, 4> filteredIm(imageHeight,imageWidth,K0.dimension(2),K0.dimension(3));
+
+    // filteredIm = convolution(image,K0);
 
     double poslin_num;
 
-    for (int l = 0; l <= K0.dimension(3) - 1; l++)
+    for (int l = 0; l <= filteredIm.dimension(3) - 1; l++)
     {
-      for (int k = 0; k <= K0.dimension(2) - 1; k++)
+      for (int k = 0; k <= filteredIm.dimension(2) - 1; k++)
       {
         for (int i = 0; i <= imageHeight - 1; i++)
         {
@@ -191,16 +200,55 @@ public:
     return posLinTensor;
   }
 
-  Eigen::Tensor<double, 4> maxPool()
+  Eigen::MatrixXd reLu(Eigen::MatrixXd filteredIm)
+  {
+    int kernelHeight,kernelWidth,imageHeight,imageWidth;
+    Eigen::MatrixXd sizeImage;
+
+    // sizeImage = extract2DMatrix(0,0,filteredIm);
+
+    imageHeight = filteredIm.rows();
+    imageWidth = filteredIm.cols();
+
+    Eigen::MatrixXd posLinTensor(imageHeight,imageWidth);
+    // Eigen::Tensor<double, 4> filteredIm(imageHeight,imageWidth,K0.dimension(2),K0.dimension(3));
+
+    // filteredIm = convolution(image,K0);
+
+    double poslin_num;
+
+
+        for (int i = 0; i <= imageHeight - 1; i++)
+        {
+          for (int j = 0; j <= imageWidth - 1; j++)
+          {
+            poslin_num = filteredIm(i,j);
+            if (poslin_num > 0)
+            {
+              posLinTensor(i,j) = filteredIm(i,j);
+            }
+            else
+            {
+              posLinTensor(i,j) = 0;
+            }
+          }
+        }
+
+    return posLinTensor;
+  }
+
+  Eigen::Tensor<double, 4> maxPool(Eigen::Tensor<double, 4> posLinIm)
   {
     int imageHeight,imageWidth;
-    imageHeight = image.rows();
-    imageWidth = image.cols();
+    Eigen::MatrixXd sizeImage;
+    sizeImage = extract2DMatrix(0,0,posLinIm);
+    imageHeight = sizeImage.rows();
+    imageWidth = sizeImage.cols();
 
-    Eigen::Tensor<double, 4> maxPoolTensor((imageHeight + imageHeight%2)/2,(imageHeight + imageHeight%2)/2,K0.dimension(2),K0.dimension(3));
-    Eigen::Tensor<double, 4> posLinIm(imageHeight,imageWidth,K0.dimension(2),K0.dimension(3));
+    Eigen::Tensor<double, 4> maxPoolTensor((imageHeight + imageHeight%2)/2,(imageHeight + imageHeight%2)/2,posLinIm.dimension(2),posLinIm.dimension(3));
+    // Eigen::Tensor<double, 4> posLinIm(imageHeight,imageWidth,K0.dimension(2),K0.dimension(3));
 
-    posLinIm = reLu();
+    // posLinIm = reLu();
 
     Eigen::MatrixXd maxPoolMatrix;
     Eigen::MatrixXd maxpool_extended(imageHeight + imageHeight%2,imageWidth + imageWidth%2);
@@ -209,9 +257,9 @@ public:
 
     double maxPoolElement;
 
-    for (int l = 0; l <= K0.dimension(3) - 1; l++)
+    for (int l = 0; l <= posLinIm.dimension(3) - 1; l++)
     {
-      for (int k = 0; k <= K0.dimension(2) - 1; k++)
+      for (int k = 0; k <= posLinIm.dimension(2) - 1; k++)
       {
         maxPoolMatrix = extract2DMatrix(k,l,posLinIm);
         maxpool_extended << maxPoolMatrix,maxpool_padding_ver,
@@ -229,7 +277,8 @@ public:
     return maxPoolTensor;
   }
 
-  Eigen::Tensor<double, 4> convolutionInnerLayer()
+
+  Eigen::Tensor<double, 4> convolutionInnerLayer(Eigen::Tensor<double, 4> maxPoolTensor)
   {
     Eigen::MatrixXd maxPoolMatrix;
     Eigen::MatrixXd kernelMatrix;
@@ -249,14 +298,14 @@ public:
       bias1[0][i] = b1(i)*onesMatrix;
     }
 
-    Eigen::Tensor<double, 4> maxPoolTensor((imageHeight + imageHeight%2)/2,(imageHeight + imageHeight%2)/2,K0.dimension(2),K0.dimension(3));
+    // Eigen::Tensor<double, 4> maxPoolTensor((imageHeight + imageHeight%2)/2,(imageHeight + imageHeight%2)/2,K0.dimension(2),K0.dimension(3));
     Eigen::Tensor<double, 4> convolutionTensor((imageHeight + imageHeight%2)/2,(imageHeight + imageHeight%2)/2,K0.dimension(2),K1.dimension(3));
     Eigen::MatrixXd convolutionArray[K0.dimension(2)][K1.dimension(2)];
     Eigen::MatrixXd convolutionArray2[K0.dimension(2)][K1.dimension(3)];
 
 
 
-    maxPoolTensor = maxPool();
+    // maxPoolTensor = maxPool();
 
     for (int j = 0; j <= K1.dimension(3) - 1; j++)
     {
@@ -271,8 +320,6 @@ public:
       convolutionArray2[0][j] = sumMatrix;
     }
 
-    // std::cout << "\nConvolution array\n" << maxPoolTensor.dimension(1);
-
     for (int l = 0; l <= K1.dimension(3) - 1; l++)
     {
       for (int k = 0; k <= K0.dimension(2) - 1; k++)
@@ -281,17 +328,82 @@ public:
         {
           for (int j = 0; j<= maxPoolTensor.dimension(1) - 1; j++)
           {
-            // element = convolutionArray2[k][l](i,j);
             convolutionTensor(i,j,k,l) = convolutionArray2[k][l](i,j) + bias1[k][l](i,j);
           }
         }
       }
     }
 
-    // std::cout << "\nConvolution array\n" << element;
-
     return convolutionTensor;
+  }
 
+  Eigen::MatrixXd reshapeToVector(Eigen::Tensor<double, 4> maxPoolIm)
+  {
+    int imageHeight,imageWidth,numberOfImages;
+    numberOfImages = maxPoolIm.dimension(3);
+    imageHeight = maxPoolIm.dimension(0);
+    imageWidth = maxPoolIm.dimension(1);
+
+
+    Eigen::MatrixXd outputVector(1,numberOfImages*imageHeight*imageWidth);
+    Eigen::MatrixXd partialVector(1,numberOfImages);
+    for (int i = 0; i <= imageHeight - 1; i++)
+    {
+      for (int j = 0; j <= imageWidth - 1; j++)
+      {
+        for (int k = 0; k <= numberOfImages - 1; k++)
+        {
+          partialVector(0,k) = maxPoolIm(i,j,0,k);
+        }
+        outputVector.block(0,numberOfImages*j + numberOfImages*imageWidth*i,1,numberOfImages) = partialVector;
+      }
+    }
+
+    return outputVector;
+  }
+
+  // Eigen::MatrixXd::Index feedForward(Eigen::MatrixXd reshapedV)
+  int feedForward(Eigen::MatrixXd reshapedV)
+  {
+    Eigen::MatrixXd weightedVector,weightedVector1;
+    Eigen::MatrixXd outputFirstFF,outputSecondFF;
+    int maxRow,maxCol;
+
+    int maxVal;
+
+    weightedVector = reshapedV*W0 + b2.transpose();
+
+
+    outputFirstFF = reLu(weightedVector);
+
+    weightedVector1 = outputFirstFF*W1 + b3.transpose();
+
+    outputSecondFF = weightedVector1;
+
+    int max = weightedVector1.maxCoeff(&maxRow,&maxCol);
+
+    // std::cout << "\n coefficient\n" << maxCol;
+
+    return maxCol;
+
+  }
+
+  int predictOutput()
+  {
+    Eigen::Tensor<double, 4> convolutionLayer1,reLuLayer1,maxPoolLayer1,convolutionLayer2,reLuLayer2,maxPoolLayer2;
+    Eigen::MatrixXd reshapedVector;
+    int prediction;
+
+    convolutionLayer1 = convolution(image,K0);
+    reLuLayer1 = reLu(convolutionLayer1);
+    maxPoolLayer1 = maxPool(reLuLayer1);
+    convolutionLayer2 = convolutionInnerLayer(maxPoolLayer1);
+    reLuLayer2 = reLu(convolutionLayer2);
+    maxPoolLayer2 = maxPool(reLuLayer2);
+    reshapedVector = reshapeToVector(maxPoolLayer2);
+    prediction = feedForward(reshapedVector);
+
+    return prediction;
   }
 
 
--- CNNCpp/MaxPlusScheduler.cpp
+++ CNNCpp/MaxPlusScheduler.cpp
@@ -4,9 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fstream>
-#include </home/octavio/Git/Personal/CNNCpp/Eigen/Dense>
-#include </home/octavio/Git/Personal/CNNCpp/unsupported/Eigen/CXX11/Tensor>
-#include </home/octavio/Git/Personal/CNNCpp/unsupported/Eigen/CXX11/Tensor>
+#include </home/octaviovillarreal/Git/Personal/CNNCpp/Eigen/Dense>
+#include </home/octaviovillarreal/Git/Personal/CNNCpp/unsupported/Eigen/CXX11/Tensor>
+#include </home/octaviovillarreal/Git/Personal/CNNCpp/unsupported/Eigen/CXX11/Tensor>
 #include <vector>
 #include <set>
 #include <string>
@@ -14,9 +14,9 @@
 #include <cstdlib>
 #include <sstream>
 
-#include </home/octavio/Git/Personal/CNNCpp/csvRead.h>
-#include </home/octavio/Git/Personal/CNNCpp/tensorbuild.h>
-#include </home/octavio/Git/Personal/CNNCpp/HyQNN.h>
+#include </home/octaviovillarreal/Git/Personal/CNNCpp/csvRead.h>
+#include </home/octaviovillarreal/Git/Personal/CNNCpp/tensorbuild.h>
+#include </home/octaviovillarreal/Git/Personal/CNNCpp/HyQNN.h>
 
 using namespace std;
 
@@ -48,6 +48,20 @@ int main ()
   value_matrix1 = kernel1.createMatrix();
   // std::cout << "\n Kernel matrix: \n" << value_matrix1;
 
+  // Obtain matrix containing kernels from csv file
+  CsvReadFile weight0;
+  Eigen::MatrixXd weight_matrix;
+  weight0.set_csv("LF-SimpCNN-ffnn_part-layer_0-W.csv");
+  weight_matrix = weight0.createMatrix();
+  // std::cout << "\n Weight 0: \n" << weight_matrix;
+
+  // Obtain matrix containing kernels from csv file
+  CsvReadFile weight1;
+  Eigen::MatrixXd weight_matrix1;
+  weight1.set_csv("LF-SimpCNN-ffnn_part-layer_1-W.csv");
+  weight_matrix1 = weight1.createMatrix();
+  // std::cout << "\n Weight 1: \n" << weight_matrix1;
+
   // Obtain vector of bias
   CsvReadFile b0;
   Eigen::MatrixXd bias0;
@@ -61,6 +75,20 @@ int main ()
   b1.set_csv("LF-SimpCNN-conv_part-layer_1-bias.csv");
   bias1 = b1.createMatrix();
   // std::cout << "\nbias 1: \n" << bias1;
+
+  // Obtain vector of bias
+  CsvReadFile b2;
+  Eigen::MatrixXd bias2;
+  b2.set_csv("LF-SimpCNN-ffnn_part-layer_0-b.csv");
+  bias2 = b2.createMatrix();
+  // std::cout << "\nbias 2: \n" << bias2;
+
+  // Obtain vector of bias
+  CsvReadFile b3;
+  Eigen::MatrixXd bias3;
+  b3.set_csv("LF-SimpCNN-ffnn_part-layer_1-b.csv");
+  bias3 = b3.createMatrix();
+  // std::cout << "\nbias 3: \n" << bias3;
 
   // Create kernel tensor
   TensorBuild kernel0Tensor;
@@ -90,7 +118,7 @@ int main ()
   Eigen::MatrixXd tensorConvolutionMatrix[size_tensor(2)][size_tensor(3)]; // Use tensor size definition to build kernel matrix
   Eigen::MatrixXd tensorConvolution_0(image_matrix.rows(),image_matrix.cols());
   Eigen::MatrixXd filtered_image_array[size_tensor(2)][size_tensor(3)];
-  neuralNetwork.set_neuralNetwork(K0,K1,bias0,bias1,image_matrix);
+  neuralNetwork.set_neuralNetwork(K0,K1,bias0,bias1,bias2,bias3,weight_matrix,weight_matrix1,image_matrix);
   tensorConvolution = neuralNetwork.convolution(image_matrix,K0);
   tensorConvolution_0 = neuralNetwork.extract2DMatrix(0,0,tensorConvolution);
   // std::cout << "\n Filtered image \n" << tensorConvolution_0;
@@ -98,7 +126,7 @@ int main ()
   // Activation function ReLu
   Eigen::MatrixXd poslin(image_size(0),image_size(1));
   Eigen::Tensor<double, 4> tensorReLu(size_tensor(0),size_tensor(1),size_tensor(2),size_tensor(3));
-  tensorReLu = neuralNetwork.reLu();
+  tensorReLu = neuralNetwork.reLu(tensorConvolution);
   poslin = neuralNetwork.extract2DMatrix(0,0,tensorReLu);
   // std::cout << "\n Filtered image with activation \n" << poslin;
 
@@ -106,7 +134,7 @@ int main ()
   Eigen::MatrixXd maxPoolMatrix;
   Eigen::MatrixXd maxpool_array[size_tensor(2)][size_tensor(3)];
   Eigen::Tensor<double, 4> tensorMaxPool(size_tensor(0),size_tensor(1),size_tensor(2),size_tensor(3));
-  tensorMaxPool = neuralNetwork.maxPool();
+  tensorMaxPool = neuralNetwork.maxPool(tensorReLu);
   maxPoolMatrix = neuralNetwork.extract2DMatrix(0,2,tensorMaxPool);
   // std::cout << "\n Matrix after maxpool \n" << maxPoolMatrix;
 
@@ -118,11 +146,40 @@ int main ()
   // std::cout << "\n Single convolution \n" << singleConvolution;
 
   // Second layer
+  // Convolution
   Eigen::Tensor<double, 4> secondLayer;
   Eigen::MatrixXd secondLayerMatrix;
-  secondLayer = neuralNetwork.convolutionInnerLayer();
-  secondLayerMatrix = neuralNetwork.extract2DMatrix(0,0,secondLayer);
-  std::cout << "\n Second layer \n" << secondLayerMatrix;
+  secondLayer = neuralNetwork.convolutionInnerLayer(tensorMaxPool);
+  secondLayerMatrix = neuralNetwork.extract2DMatrix(0,7,secondLayer);
+  // std::cout << "\n Second layer \n" << secondLayerMatrix;
+
+  // ReLu
+  Eigen::MatrixXd poslin2;
+  Eigen::Tensor<double, 4> tensorReLu2(secondLayer.dimension(0),secondLayer.dimension(1),secondLayer.dimension(2),secondLayer.dimension(3));
+  tensorReLu2 = neuralNetwork.reLu(secondLayer);
+  poslin2 = neuralNetwork.extract2DMatrix(0,7,tensorReLu2);
+  // std::cout << "\n Filtered image with activation \n" << poslin2;
+
+  // maxPool
+  Eigen::MatrixXd maxPoolMatrix2;
+  Eigen::Tensor<double, 4> tensorMaxPool2;
+  tensorMaxPool2 = neuralNetwork.maxPool(tensorReLu2);
+  maxPoolMatrix2 = neuralNetwork.extract2DMatrix(0,0,tensorMaxPool2);
+  // std::cout << "\n Matrix after maxpool \n" << maxPoolMatrix2;
+
+  // Reshape
+  Eigen::MatrixXd reshapeVector;
+  reshapeVector = neuralNetwork.reshapeToVector(tensorMaxPool2);
+  // std::cout << "\n Reshape vector \n" << reshapeVector;
+
+  //Feedforward layer
+  Eigen::MatrixXd::Index Feedforward;
+  Feedforward = neuralNetwork.feedForward(reshapeVector);
+  // std::cout << "\n Feedforward \n" << Feedforward;
+
+  int prediction;
+  prediction = neuralNetwork.predictOutput();
+  std::cout << "\n Prediction: " << prediction;
 
 
   return 0;
